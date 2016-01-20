6b96b749ea84070802dec47246b996ec714c8abb
Krzysztof Garczynski <garekkream@gmail.com>
2016-01-21 00:57:59 +0100

    Server initialization

--- scope_server.c
+++ scope_server.c
@@ -23,19 +23,58 @@ static struct sockaddr_in serv;
 int server_init(void)
 {
 	if(server_status != SERVER_STATUS_STOPPED) {
-		
+		syslog(LOG_ERR, "Server is still running! Initialization failed!\n");
 		return -EALREADY;
 	}
+
+	server_status = SERVER_STATUS_STOPPED;
+
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+	if(socket_desc < 0) {
+		syslog(LOG_ERR, "Cannot create socket descriptor! Initialization failed!\n");
+		return -EINVAL;
+	}
+
+	serv.sin_family = AF_INET;
+	serv.sin_addr.s_addr = INADDR_ANY;
+	serv.sin_port = htons(6633);
+
+	if(bind(socket_desc, (struct sockaddr *)&serv, sizeof(serv)) < 0) {
+		syslog(LOG_ERR, "Cannot bind to port! Initialization failde!\n");
+		return -EINVAL;
+	}
+
+	listen(socket_desc, 3);
+
+	syslog(LOG_INFO, "Server initialized!\n");
+
 	return 0;
 }
 
 int server_start(void)
 {
+	struct sockaddr_in client;
+	int socket_new;
+	int client_len = sizeof(client);
+
+	server_status = SERVER_STATUS_RUNNING;
+
+	syslog(LOG_INFO, "Server started! (Version =%s)\n", (char *)(__VERSION_TAG));
+
+	while((socket_new = accept(socket_desc, (struct sockaddr *)&serv, (socklen_t *)&(client_len))) && (server_status != SERVER_STATUS_STOPPED)) {
+	
+	}
+
+	syslog(LOG_INFO, "Exiting\n");
 
 	return 0;
 }
 
 void server_stop(void)
 {
+	server_status = SERVER_STATUS_STOPPED;
 
+	syslog(LOG_INFO, "Server stopped!\n");
+
+	close(socket_desc);
 }
--- scope_server.h
+++ scope_server.h
@@ -4,4 +4,11 @@
 #define SCOPE_FILE_PID		"/var/run/scope_daemon.pid"
 #define SCOPE_FILE_FIFO		"/tmp/scope_fifo"
 
+#define SERVER_STATUS_STOPPED	0
+#define SERVER_STATUS_RUNNING	1
+
+extern int server_init(void);
+extern int server_start(void);
+extern void server_stop(void);
+
 #endif //SCOPE_SERVER_H
